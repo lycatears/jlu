992efcb033486ebef1e80fd0e28985b348522ba2
ly <lycatears@outlook.com>
2026-05-20 12:39:57 +0800

    添加作业1的代码

--- 程序设计基础/作业代码/0-1.c
+++ 程序设计基础/作业代码/0-1.c
@@ -1,33 +1,19 @@
 /*第一个C程序。
-
 问题描述：通常学习一门编程语言，开始的第一个程序就是将“Hello World”显示在屏幕上。请大家参照如下代码的样子，写出自己的第一个C语言程序。亲，一定要自己写，直接粘贴是练不到的哦！一定注意字母大小写！
-
 提示：
-
 1.程序语言请选择C/C++或DEV C++；
-
 2.main函数的返回类型一定要是整型且不可省略。
-
 #include <stdio.h>
-
 int main() {
-
 printf("Hello World");
-
 return 0;
-
 }
-
 输入: 无
-
 输出：Hello World
 */
 #include <stdio.h>
-
 int main()
 {
-
     printf("Hello World");
-
     return 0;
 }
--- 程序设计基础/作业代码/0-2.c
+++ 程序设计基础/作业代码/0-2.c
@@ -1,52 +1,29 @@
 /*
 求两个给定整数之和。
-
 问题描述：以下程序实现在屏幕上输出两个给定整数之和，请补全如下代码。
-
 提示：
-
 1.请仔细看样例，输出结果33的前后没有空格、换行等字符，程序中不要随意添加哦；
-
 2.提交时需拷贝补全后的完整程序代码，不能只拷贝补充的代码行哦。
-
 #include <stdio.h>
-
 int main(void){
-
    int data1,data2;//用于存储给定的2个整数
-
    data1=13;//为data1赋值
-
    data2=data1+7;//为data2赋值
-
    printf(                         );//补全括号内的代码，输出两个整数之和
-
    return 0;
-
 }
-
 输入：无
-
 输出：2个给定整数的和
-
 样例1：
-
 输入：无
-
 输出：33
 */
 #include <stdio.h>
-
 int main(void)
 {
-
     int data1, data2;
-
     data1 = 13;
-
     data2 = data1 + 7;
-
     printf("%d", data1 + data2);
-
     return 0;
 }
