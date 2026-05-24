daafc9690d3b2a52d3fea2d7b3041e871cd634ed
ly <lycatears@outlook.com>
2026-05-24 23:32:13 +0800

    添加操作系统实验代码

---
+++ 操作系统/实验课代码/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+    printf("hello world!\n");
+    double aaa=sin(3.14);
+    printf("%lf",aaa);
+    char ccc=getchar();
+    putchar(ccc);
+    return 0;
+}
